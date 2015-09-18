e5afd045eb10e511131c3b1c13a8a9b85e545f2b
Benjamin Legrand <benjilegnard@protonmail.com>
2015-09-18 18:13:42 +0200

    browserify for front-end dependencies.

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var watchify = require('watchify');
 var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
@@ -9,23 +10,32 @@ var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 var config = require('../config');
 var connect = require('gulp-connect');
+var assign = require('lodash').assign;
 
 
-gulp.task('javascript', function () {
     // set up the browserify instance on a task basis
-    var b = browserify({
-        entries: 'zelda.min.js',
+    var browserifyOptions = {
+        entries: [config.src + '/js/zelda.js'],
         debug: true
-    });
+    };
+var opts = assign({}, watchify.args, browserifyOptions);
+var bro = watchify(browserify(opts));
 
-    return b.bundle()
-        .pipe(source(config.src+'/js/zelda.js'))
+function bundle() {
+    return bro.bundle()
+        // log errors if they happen
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('zelda.min.js'))
+        // optional, remove if you don't need to buffer file contents
         .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
+        // optional, remove if you dont want sourcemaps
+        .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
         // Add transformation tasks to the pipeline here.
         .pipe(uglify())
-        .on('error', gutil.log)
-        .pipe(sourcemaps.write(config.dest+'/js/'))
-        .pipe(gulp.dest(config.dest+'/js/'))
-        .pipe(connect.reload());
-});
+        .pipe(sourcemaps.write('./')) // writes .map file
+        .pipe(gulp.dest('./public/js/'));
+}
+
+gulp.task('javascript', bundle);
+bro.on('update', bundle); // on any dep update, runs the bundler
+bro.on('log', gutil.log); // output build logs to terminal
