f444e5e9f97ad719495f4c71afd914de348cb0e6
Benjamin Legrand <jibhaine@gmail.com>
2015-09-18 18:13:42 +0200

    browserify for front-end dependencies.

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var requireDir = require('require-dir');
+
+//full tasks are defined in ./gulp/tasks/*.js individual files.
+requireDir('./gulp/tasks');
+
+//only composite tasks here
+gulp.task('build', ['less', 'browserify']);
+gulp.task('default',['build','serve','watch']);
+
--- public/js/zelda.js
+++ public/js/zelda.js
@@ -1,66 +1,13 @@
-// Sets the require.js configuration for your application.
-require.config({
-    baseUrl:'/js',
-    // 3rd party script alias names (Easier to type "jquery" than "libs/jquery-1.7.2.min")
-    paths: {
-
-        // Core Libraries
-        "modernizr":      "libs/modernizr",
-        "html5shiv":      "libs/html5shiv",
-        "jquery":         "libs/zepto",
-        "underscore":         "libs/lodash",
-        "backbone":         "libs/backbone",
-
-        "hammer":         "libs/hammer",
-        "less":      "libs/less",
-
-        //plugins require
-        "async": "libs/require/async",
-        "font": "libs/require/font",
-        "image": "libs/require/image",
-        "depend": "libs/require/font",
-        "goog": "libs/require/image",
-        "propertyParser": "libs/require/propertyParser",
-        "domReady":       "libs/require/domReady",
-        "text": "libs/require/text",
+var $ = require('jquery');
+var Backbone = require('backbone');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+var Phaser = require('phaser');
+var GameRouter = require('./zelda/routers/GameRouter');
+Backbone.$ = $;
 
 
-        "pixi": "libs/pixi"
-
-    },
-
-    // Sets the configuration for your third party scripts that are not AMD compatible
-    shim: {
-
-        "pixi": {
-            "exports": "PIXI"
-        },
-
-        "underscore": {
-            "exports": "_"  //attaches "_" to the window object
-        },
-
-        "backbone": {
-            "deps": ["underscore", "jquery"],
-            "exports" : "Backbone"  //attaches "Backbone" to the window object
-        }
-
-    } // end Shim Configuration
-
-});
-
-// Include Desktop Specific JavaScript files here (or inside of your Desktop router)
-require(
-    [
-        "jquery",
-        "pixi",
-        "pixi-example",
-        "underscore",
-        "backbone",
-        "modernizr",
-        "zelda/routes/GameRouter"
-    ],
-    function( $, PIXI, Backbone, _ , GameRouter) {
+(function( $, Phaser, Backbone, GameRouter) {
         "use strict";
         var Zelda = {
             Models:[],
@@ -69,6 +16,7 @@ require(
         };
         // Instantiates a new Router
         this.router = new GameRouter();
+        Backbone.history.start();
         window.Zelda = Zelda;
         return Zelda;
-    });
+})($,  Phaser, Backbone, GameRouter);
