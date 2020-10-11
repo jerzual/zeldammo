8bca541a4ac0f7eca58706edd860c264585f1131
Benjamin Legrand <benjilegnard@protonmail.com>
2020-10-11 20:16:56 +0200

    feat(upgrade-stack): remove gulp and browserify

--- gulp/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    server: {},
-    src: './public/',
-    dest: './public/'
-};
--- gulp/tasks/watch.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const config = require('../config');
-
-gulp.task('watch', function() {
-    gulp.watch([config.src + '/less/*.less'], ['less']);
-    gulp.watch([config.src + '/js/**/*.js'], ['browserify']);
-        gulp.watch([config.src + '/styles/*.scss'], ['sass']);
-        gulp.watch([config.src + '/js/**/*.js'], ['rollup']);
-});
