18187371622c0530df0cb369eeac22364a15b261
Benjamin Legrand <benjilegnard@protonmail.com>
2023-07-31 18:45:12 +0200

    feat: move to vite and esbuild, upgrade node

--- server/app.ts
+++ server/app.ts
@@ -1,22 +1,31 @@
 import * as express from "express";
-import { SocketIO } from "socket.io";
-import { from } from "rxjs";
-import { join } from "path";
-import { createServer } from "http";
-import { Server } from "@colyseus/core";
-import { WebSocketTransport } from "@colyseus/ws-transport"
-const port = Number(process.env.PORT) || 3000;
-
+import { join } from "node:path";
+import { createServer } from "node:http";
+import { LobbyRoom, Server } from "@colyseus/core";
+import { WebSocketTransport } from "@colyseus/ws-transport";
+import pino from "pino";
+import pinoHttp from "pino-http";
+const port = Number(process.env.PORT) || 2567;
+const logger = pino();
 // express main app
-const app = new express();
-app.use(express.static(join(__dirname, '..','client')));
+const app = express.default();
+
+app.use(pinoHttp);
+// app.use(express.static(join(process.cwd(), "dist", "render")));
 
 // colyseus game server
 const gameServer = new Server({
   transport: new WebSocketTransport({
-  server: createServer(app)
-})
+    server: createServer(app),
+  }),
+  greet: false,
 });
-
-
+gameServer.define("lobby", LobbyRoom);
 gameServer.listen(port);
+
+gameServer.onShutdown(() => {
+  logger.info("server shutdown");
+});
+app.listen(3000, () => {
+  logger.info("express server started");
+});
